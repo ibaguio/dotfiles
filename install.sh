b930f34be25ae60782f7da941ce643209191fae2
ibaguio <baguio.ivan@gmail.com>
2016-05-05 18:37:04 +0800

    updated install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ ln -s `pwd`/tmux.conf ~/.tmux.conf
 ln -s `pwd`/vimrc ~/.vimrc
 
 mkdir -p ~/.vim/bundle/
+mkdir -p ~/.vim/autoload/
 
+# install pathogen
+curl -o ~/.vim/bundle/autoload/  https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# install other plugins
 git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/
 git clone https://github.com/klen/python-mode ~/.vim/bundle/
 git clone https://github.com/scrooloose/nerdcommenter ~/.vim/bundle/
@@ -13,3 +18,6 @@ git clone https://github.com/ntpeters/vim-better-whitespace ~/.vim/bundle/
 git clone https://github.com/Lokaltog/vim-powerline ~/.vim/bundle/
 git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/
 
+# source personal rc-file
+echo "source ~/.dotfiles/ibaguio-rc" >> ~/.bashrc
+echo "source ~/.dotfiles/ibaguio-rc" >> ~/.zshrc
