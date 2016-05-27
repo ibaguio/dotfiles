60a70326a0d145c0d46d7e7a7573634edd2fe895
ibaguio <baguio.ivan@gmail.com>
2016-05-27 14:06:44 +0800

    fixed bugs on install script

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Install dot files
 
+# copy this directory to ~/.dotfiles
+cp -r `pwd` ~/.dotfiles
+cd ~/.dotfiles/
+
 # just create symlinks to this dir
-ln -s `pwd`/tmux.conf ~/.tmux.conf
-ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/psqlrc ~/.psqlrc
+ln -sf `pwd`/tmux.conf ~/.tmux.conf
+ln -sf `pwd`/vimrc ~/.vimrc
+ln -sf `pwd`/psqlrc ~/.psqlrc
 
 mkdir -p ~/.vim/bundle/
 mkdir -p ~/.vim/autoload/
@@ -16,18 +20,23 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
     brew install automake pkg-config pcre xz
 fi
 # install pathogen
-curl -o ~/.vim/bundle/autoload/  https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -o ~/.vim/autoload/pathogen.vim
 
+# install molokai colors
+mkdir -p ~/.vim/colors
+curl https://github.com/tomasr/molokai.vim -o ~/.vim/colors/molokai.vim
+
+cd ~/.vim/bundle
 # install other plugins
-git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/
-git clone https://github.com/klen/python-mode ~/.vim/bundle/
-git clone https://github.com/scrooloose/nerdcommenter ~/.vim/bundle/
-git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/
-git clone https://github.com/ntpeters/vim-better-whitespace ~/.vim/bundle/
-git clone https://github.com/Lokaltog/vim-powerline ~/.vim/bundle/
-git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/
-git clone http://github.com/sjl/gundo.vim.git ~/.vim/bundle/
-git clone https://github.com/rking/ag.vim ~/.vim/bundle/
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/klen/python-mode
+git clone https://github.com/scrooloose/nerdcommenter
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/ntpeters/vim-better-whitespace
+git clone https://github.com/Lokaltog/vim-powerline
+git clone https://github.com/airblade/vim-gitgutter
+git clone http://github.com/sjl/gundo.vim.git
+git clone https://github.com/rking/ag.vim
 
 # source personal rc-file
 echo "source ~/.dotfiles/ibaguio-rc" >> ~/.bashrc
